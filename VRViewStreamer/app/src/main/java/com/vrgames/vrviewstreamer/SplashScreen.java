9782870670631e9fe8fea568a25a2e6cf5914d8a
ahmetozdemirrr <ao7252167@gmail.com>
2024-04-09 03:11:13 +0300

    Adding android part introduction

---
+++ VRViewStreamer/app/src/main/java/com/vrgames/vrviewstreamer/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.vrgames.vrviewstreamer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.vrgames.vrviewstreamer.MainActivity;
+
+public class SplashScreen extends AppCompatActivity
+{
+    private static final int SPLASH_SCREEN_DURATION = 2500; // Başlangıçta ilk ekranda 2 saniye duralım.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                // Splash ekranı gösterildikten sonra ana aktiviteye geçiş yap
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Splash aktivitesini kapat
+            }
+        }, SPLASH_SCREEN_DURATION);
+    }
+}
